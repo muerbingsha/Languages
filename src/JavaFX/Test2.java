c89601653457ff871cc4b79d4fe6335a697da70c
Shark <u6849956@anu.edu.au>
2019-04-29 14:21:05 +1000

    Retry IntelliJ

---
+++ src/JavaFX/Test2.java
@@ -0,0 +1,43 @@
+package JavaFX;
+
+import javafx.animation.*;
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+
+public class Test2 extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("Test 2");
+
+        // set scene
+        Group root = new Group();
+        Scene scene = new Scene(root, 300, 300);
+        stage.setScene(scene);
+
+        // add rectangle
+        Rectangle rec = new Rectangle(0, 0, 50, 50);
+        root.getChildren().add(rec);
+
+        // set animation key value
+        KeyValue kv1 = new KeyValue(rec.opacityProperty(), 0);
+        KeyValue kv2 = new KeyValue(rec.xProperty(), 100, Interpolator.EASE_OUT);
+        KeyFrame kf = new KeyFrame(Duration.millis(1000), kv1, kv2);
+
+        Timeline time = new Timeline();
+        time.setAutoReverse(true);
+        time.setCycleCount(Animation.INDEFINITE);
+
+        time.getKeyFrames().add(kf);
+
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/JavaTest/TestJunit.java
@@ -0,0 +1,20 @@
+package JavaTest;
+
+/*
+ * This is for Junit
+ */
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class TestJunit {
+
+    @Test
+    public void testEqual(){
+        String a = "Good";
+        assertEquals("Good", a);
+    }
+
+
+
+}
+
